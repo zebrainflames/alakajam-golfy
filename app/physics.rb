51948c7c020ef6884a4505d93cb041dfa38ea0f3
jhkroger <jhkroger2@gmail.com>
2025-02-23 14:27:52 +0200

    tuning physics feel

--- app/physics.rb
+++ app/physics.rb
@@ -1,24 +1,54 @@
 # frozen-string-literal: true
 
+GRAVITY_SCALE = 2.7 # this is a magic scaling number to tune gravity strength and thus game feel
+
+def swing_direction(dynamic_body, args)
+  drag_x = dynamic_body.swing_start_x - args.inputs.mouse.x
+  drag_y = dynamic_body.swing_start_y - args.inputs.mouse.y
+  drag_distance = Math.sqrt(drag_x**2 + drag_y**2)
+
+  # we use a non-linear power function for a nicer game feel; enough power on small drags to
+  # get moving but long drags don't send us out into the void of space
+  swing_power = (drag_distance**1.2) * 0.1 # ~magic~ numbers again 8)
+
+  if drag_distance > 0.01
+    dir_x = drag_x / drag_distance
+    dir_y = drag_y / drag_distance
+  else 
+    dir_x = 0
+    dir_y = 0
+  end
+
+  [dir_x * swing_power, dir_y * swing_power]
+end
+
+def swing_ball(dynamic_body, args)
+  dx, dy = swing_direction(dynamic_body, args)
+
+  dynamic_body.vx = dx
+  dynamic_body.vy = dy
+end
+
 def apply_gravity(planets, dynamic_body, dt)
   planets.each do |planet|
     dx = planet.x - dynamic_body.x
     dy = planet.y - dynamic_body.y
     r2 = dx**2 + dy**2
     r3 = r2 * Math.sqrt(r2)
-    dynamic_body.vx += dx * planet.mass / r3 * dt
-    dynamic_body.vy += dy * planet.mass / r3 * dt
+    dynamic_body.vx += dx * planet.mass / r3 * dt * GRAVITY_SCALE
+    dynamic_body.vy += dy * planet.mass / r3 * dt * GRAVITY_SCALE
   end
 end
 
 def physics_update(planets, dynamic_body)
+  return if dynamic_body.swing_state == :waiting_for_swing
   # DR uses a fixed timestep of 1/60 seconds; this is just the delta time value for
   # the euler integraiton in this simulation (called every frame)
   dt = 1.0 / 30.0
 
   apply_gravity(planets, dynamic_body, dt)
 
-  # do collisions checks; it's done as a separate loop to separate velocity updates from gravity:E
+  # do collisions checks; it's done as a separate loop to separate velocity updates from gravity
   # and collisions to get them right. If they're done in the same loop, either the ball tunnels into bodies,
   # or some planet's gravity is not applied correctly, for example!
   bounced = false
@@ -29,6 +59,7 @@ def physics_update(planets, dynamic_body)
     combined_radius = planet.radius + dynamic_body.radius
     next unless r2 < combined_radius**2 # skip to next planet on no collision
 
+    #puts "COLLISION"
     bounced = true
     dist = Math.sqrt(r2)
     overlap = dist - combined_radius
@@ -40,10 +71,25 @@ def physics_update(planets, dynamic_body)
     normal_x = dx / dist
     normal_y = dy / dist
     dot_product = dynamic_body.vx * normal_x + dynamic_body.vy * normal_y
-    # NOTE: this friction/bounce amount could come from planet definition, or be a global constant
-    bounce_factor = 0.73
+    
+    bounce_factor = 0.83 # some ~magic~ numbers to tune the bounce
     dynamic_body.vx -= 2 * dot_product * normal_x * bounce_factor
     dynamic_body.vy -= 2 * dot_product * normal_y * bounce_factor
+
+    # apply some friction to the ball to slow it down for decent gameplay
+    #bounce_friction = 0.9
+    #dynamic_body.vx *= bounce_friction
+    #dynamic_body.vy *= bounce_friction
+  end
+
+  # if we bounced, apply friction to simulate rolling on the ground etc.
+  if bounced
+    bounce_friction = 0.9
+    dynamic_body.vx *= bounce_friction
+    dynamic_body.vy *= bounce_friction
+    # also add some angular velocity in the direction of movement to make it look like we're simulating
+    # rolling physics
+
   end
 
   # TODO: emit signal for collision sound effect and ball state changes here instead of setting state directly
@@ -52,6 +98,13 @@ def physics_update(planets, dynamic_body)
   if dynamic_body.swing_state && bounced && dynamic_body.swing_state != :ready
     dynamic_body.swing_state = :ready
     puts 'Ball ready to be hit again!'
+  end
+
+  # fake 'air' drag to slow down the ball in case it does not hit anything (to prevent stable orbits)
+  if !bounced && dynamic_body.swing_state == :flying
+    air_drag = 0.99999
+    dynamic_body.vx *= air_drag
+    dynamic_body.vy *= air_drag
   end
 
   dynamic_body.x += dynamic_body.vx * dt
--- app/planets.rb
+++ app/planets.rb
@@ -65,9 +65,20 @@ class PlanetsScene < Scene
     reset_planets(args)
   end
 
-  def update(_args)
-    
-    physics_update(@planets, @golf_ball)
+  def ball_out_of_bounds?(args)
+    @golf_ball.x < 0 || @golf_ball.x > args.grid.w || @golf_ball.y < 0 || @golf_ball.y > args.grid.h
+  end
+
+  def update(args)
+    player_input(args)
+    physics_update(@planets, @golf_ball) if can_update_physics?
+
+    if ball_out_of_bounds?(args)
+      # TODO: play sound cue
+      # todo: display some sort of particle fx at pont of impact
+      # todo: increase stroke counter
+      reset_planets(args)
+    end
   end
 
   def render(args)
@@ -80,14 +91,66 @@ class PlanetsScene < Scene
     args.outputs.sprites << @planets.map { |planet| planet.merge(PLANET_MED_ROCK) }
 
     args.outputs.sprites << @golf_ball.merge(BALL_SPRITE)
+
+    # draw a line from ball to mouse cursor if player is dragging
+    if @golf_ball.swing_state == :swinging
+      # we just use swing power function from the physics sim -> we'll display the ball's location after
+      # one tick _if_ no other forces are applied
+      dx, dy = swing_direction(@golf_ball, args)
+      args.outputs.lines << [@golf_ball.x, @golf_ball.y, @golf_ball.x + dx, @golf_ball.y + dy, 255, 255, 255]
+    end
   end
 
   def reset_planets(args)
     @planets = []
     @planets << { x: args.grid.w / 2.0, y: args.grid.h / 2.0, mass: 120_000.0, radius: 64.0, type: :rock }
 
-    @golf_ball = { x: args.grid.w / 2.0 + 100.0, y: args.grid.h / 2.0 + 100.0, vx: 0.0, vy: 0.0, radius: 11.0,
+    @golf_ball = { x: args.grid.w / 2.0 + 100.0, y: args.grid.h / 2.0 + 100.0, vx: 0.0, vy: 0.0, av: 0.0, radius: 11.0,
                    mass: 1.0, swing_state: :waiting_for_swing }
+    @ball_flying_for = 0
+  end
+
+  private 
+
+  def can_update_physics?
+    @golf_ball.swing_state == :ready || @golf_ball.swing_state == :flying
+  end
+
+  def can_swing?
+    @golf_ball.swing_state == :waiting_for_swing || @golf_ball.swing_state == :ready
+  end
+
+  def player_input(args)
+    return if Kernel.tick_count <= 5 # ugly hacks to ignore clicks from resetting game
+    # detect if player is dragging mouse
+    if args.inputs.mouse.click && can_swing?
+      puts "Drag started"
+      @golf_ball.swing_state = :swinging
+      @golf_ball.swing_start_x = args.inputs.mouse.x
+      @golf_ball.swing_start_y = args.inputs.mouse.y
+    elsif args.inputs.mouse.click && @golf_ball.swing_state == :flying
+      puts "Ball in flight, can't swing"
+      # TODO: play sound cue
+    end
+
+    if args.inputs.mouse.up
+      puts "Drag ended"
+      if @golf_ball.swing_state == :swinging
+        @golf_ball.swing_state = :flying
+        swing_ball(@golf_ball, args)
+        # TODO: play sound cue
+      end
+    end
+
+    if args.inputs.mouse.down
+      puts "Dragging..."
+      #if @golf_ball.swing_state == :waiting_for_swing
+      #  @golf_ball.swing_state = :swinging
+      #  @golf_ball.swing_end_x = args.inputs.mouse.x
+      #  @golf_ball.swing_end_y = args.inputs.mouse.y
+      # TODO: play sound cue
+      #end
+    end
   end
 end
 
